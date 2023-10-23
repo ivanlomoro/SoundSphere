3811e3a6486c081a4b71180758cecb8d6f4eb62b
Ivan Martin Lomoro <ivanmartin_95@hotmail.com>
2023-10-23 18:17:33 +0200

    [Delete] : loginPage

--- src/routes/RouterPaths.routes.tsx
+++ src/routes/RouterPaths.routes.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react"
 import { Navigate, Route, Routes } from "react-router-dom"
 import { Home } from "../pages/Home"
-import { LandingPage, LoginPage, UserPage } from "../pages"
-import { DISPLAYPAGE, HOME, LANDINGPAGE, LOGIN, USERPAGE } from "./paths"
+import { LandingPage, UserPage } from "../pages"
+import { DISPLAYPAGE, HOME, LANDINGPAGE, USERPAGE } from "./paths"
 import { ButtonTest } from "../pages/ButtonTest"
 import { DisplayPage } from "../pages/DisplayPage"
 
@@ -15,7 +15,6 @@ export const RouterPaths: FC = () => {
             <Routes>
                 <Route path={HOME} element={<Home />} />
                 <Route path={LANDINGPAGE} element={<LandingPage />} />
-                <Route path={LOGIN} element={<LoginPage />} />
                 <Route path={USERPAGE} element={<UserPage />} />
                 <Route path={DISPLAYPAGE} element={<DisplayPage />} />
                 <Route path="/button" element={<ButtonTest />} />
