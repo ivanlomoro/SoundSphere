9262d916146fff948cfe26eecc0c6913a5e25d54
Ivan Martin Lomoro <ivanmartin_95@hotmail.com>
2023-10-23 18:11:18 +0200

    [Add]: DisplayPage

--- src/routes/RouterPaths.routes.tsx
+++ src/routes/RouterPaths.routes.tsx
@@ -2,26 +2,28 @@ import { FC } from "react"
 import { Navigate, Route, Routes } from "react-router-dom"
 import { Home } from "../pages/Home"
 import { LandingPage, LoginPage, UserPage } from "../pages"
-import { HOME, LANDINGPAGE, LOGIN, USERPAGE } from "./paths"
+import { DISPLAYPAGE, HOME, LANDINGPAGE, LOGIN, USERPAGE } from "./paths"
 import { ButtonTest } from "../pages/ButtonTest"
+import { DisplayPage } from "../pages/DisplayPage"
 
 
 
-export const RouterPaths: FC= () => {
+export const RouterPaths: FC = () => {
     return (
         <>
-     
-            <Routes>
-                <Route path= {HOME} element={<Home />} />
-                <Route path={LANDINGPAGE} element={<LandingPage/>} />
-                <Route path={LOGIN} element={<LoginPage/>} />
-                <Route path={USERPAGE} element={<UserPage/>} />
-                <Route path="/button" element={<ButtonTest/>}/>
 
-             
+            <Routes>
+                <Route path={HOME} element={<Home />} />
+                <Route path={LANDINGPAGE} element={<LandingPage />} />
+                <Route path={LOGIN} element={<LoginPage />} />
+                <Route path={USERPAGE} element={<UserPage />} />
+                <Route path={DISPLAYPAGE} element={<DisplayPage />} />
+                <Route path="/button" element={<ButtonTest />} />
+
+
                 <Route path="/" element={<Navigate to={HOME} />} />
             </Routes>
-     
+
 
         </>
     )
--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -2,6 +2,7 @@ export const LOGIN = "/loginPage"
 export const HOME = "/home"
 export const LANDINGPAGE = "/landingPage"
 export const USERPAGE = "/userPage"
+export const DISPLAYPAGE = "/displayPage"
 
 
 
