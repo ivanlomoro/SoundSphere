88e2597db5169b57e0761a2ad4e82520821415c3
Ferran Cipres <ferrancipres@gmail.com>
2023-10-23 19:13:13 +0200

    arrowback

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,4 +1,5 @@
 import { NavBar } from "../components/NavBar/NavBar"
+import { HeaderSection } from "../components/header/Header"
 import { Button } from "../components/button/Button"
 import { Container } from "../components/container/Container"
 import { EditUserForm } from "../components/editUserForm/EditUserForm"
@@ -8,6 +9,7 @@ export const UserPage = () => {
   return (
     <Container>
       <main>
+        <HeaderSection />
         <WelcomeUserSection />
         <EditUserForm />
         <Button content="Log out"/>
