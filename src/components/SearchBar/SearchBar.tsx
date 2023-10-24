f6182862c5e097d9c9df85b295700071b8c5e5e1
Ferran Cipres <ferrancipres@gmail.com>
2023-10-24 12:44:53 +0200

    update searchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -3,17 +3,32 @@ import { BiSearch } from "react-icons/bi"
 import { Link, useSearchParams } from "react-router-dom"
 import db from "../../data/db.json"
 import { Songs } from "../../Types/SongsTypes"
-
-
+import styled from "styled-components"
 
 export const SearchBar = () => {
   const [searchParams, setSearchParams] = useSearchParams()
   const [songs, setSongs] = useState<Songs[]>([])
 
+  //aplicar estilos...
+  const StyledDivSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    with: 100%;
+    margin: 10px; 
+    outline: 1px solid red;
+`
+  const StyledInputSection = styled.input `
+    margin: 10px;
+    padding: 10px;
+    padding-right: 150px;
+  `
+
   useEffect(() => {
     setSongs(db.songData)
-    console.log(songs)
   }, [])
+
+  console.log(songs)
 
   const handleQuery = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchParams({ q: e.target.value })
@@ -25,14 +40,15 @@ export const SearchBar = () => {
     <div>
       <div>
         <BiSearch />
-        <input
-          type="search"
+        <StyledInputSection
+          type="text"
           placeholder="Enter song name..."
           value={query}
           onChange={handleQuery}
         />
         {query && (
-          <div>
+          //Aqui cambios 
+          <StyledDivSection>  
             {songs
               .filter((song: Songs) => {
                 return (
@@ -42,12 +58,13 @@ export const SearchBar = () => {
               .map((song: Songs) => (
                 <Link key={song.id} to={`/songs/${song.name}`}>
                   <div>
+                    <img src={song.thumbnail} alt="miscojones" />
                     <p>{song.name}</p>
                     <p>{song.artist}</p>
                   </div>
                 </Link>
               ))}
-          </div>
+          </StyledDivSection>
         )}
       </div>
     </div>
