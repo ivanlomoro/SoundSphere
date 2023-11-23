ca17ad0e16285fe8449311eade400067d78e7329
Ignacio Velazquez <ji.velazquezpp@gmail.com>
2023-11-23 11:52:53 +0100

    add change password trigger to user details

--- src/components/userDetails/UserDetails.tsx
+++ src/components/userDetails/UserDetails.tsx
@@ -2,12 +2,36 @@ import styled from "styled-components";
 import { Button } from "../button/Button";
 import { UserDetail } from "../userDetail/UserDetail";
 import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 
 const StyledUserDetailsContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: var(--space-md);
 `;
+
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+const resetPassword = async (userEmail: string) => {
+  console.log(userEmail);
+  try {
+    const response = await axios.post(
+      "https://dev-qdcgiaoq1b8m6fe5.us.auth0.com/dbconnections/change_password",
+      {
+        email: userEmail,
+        connection: "Username-Password-Authentication",
+        client_id: "3eb0bceRj7012DEV06qrZ2AyUZdQX04L",
+      }
+    );
+    console.log(response);
+  } catch {
+    console.log("That didn't work");
+  }
+};
 
 export const UserDetails = () => {
   const { user } = useAuth0();
@@ -18,7 +42,13 @@ export const UserDetails = () => {
       <UserDetail label="Email" info={user.email} />
       <UserDetail label="Date of Birth" info={user.birthdate} />
       <UserDetail label="Gender" info={user.gender} />
-      <Button content="Change password" variant="StyledButtonPill" />
+      {user.email && (
+        <Button
+          content="Change password"
+          variant="StyledButtonPill"
+          onClick={() => resetPassword(user.email)}
+        />
+      )}
     </StyledUserDetailsContainer>
   ) : null;
 };
