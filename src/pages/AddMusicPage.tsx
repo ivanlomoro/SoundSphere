3a31ee1aa1c86a9e8c4477bea554645416d87b59
Ferran Cipres <ferrancipres@gmail.com>
2023-11-24 13:35:45 +0100

    create new page AddMusicPage

---
+++ src/pages/AddMusicPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HeaderSection } from "../components";
+
+export const AddMusicPage = () => {
+  return (
+    <section>
+        <HeaderSection text="Add Music" />
+        <div>AddMusicPage</div>
+    </section>
+
+  )
+}
