845ff666e52cb902936ef17dd47fe89920c93973
Ignacio Velazquez <ji.velazquezpp@gmail.com>
2023-11-27 14:49:24 +0100

    [FIX]: postSongs

--- src/context/songContext/ApiCalls.tsx
+++ src/context/songContext/ApiCalls.tsx
@@ -1,63 +1,64 @@
-import React, {  createContext, ReactNode, useContext } from "react";
+import React, { createContext, ReactNode, useContext } from "react";
 import axios from "axios";
 
 interface SongUploadData {
-    name: string;
-    artistId: string;
-    genreId: string;
-    url: string;
-    isPublic: boolean;
-    userCreator: string;
-    thumbnail: string;
+  name: string;
+  genreId: string;
+  url: string;
+  isPublic: boolean;
+  userCreator: string;
+  thumbnail: string;
 }
 
 interface ApiCallContextType {
-    uploadSong: (songData: SongUploadData) => Promise<void>;
+  uploadSong: (songData: SongUploadData) => Promise<void>;
 }
 
 const ApiCallsContext = createContext<ApiCallContextType | null>(null);
 
-
 const baseUrl = `http://localhost:8080`;
 
 type ProviderProps = {
-    children: ReactNode;
+  children: ReactNode;
 };
 
 const ApiCallsProvider: React.FC<ProviderProps> = ({ children }) => {
-    const uploadSong = async (songData: SongUploadData) => {
-        const userID = "6562012b45ac8690c4290edc";
-        const encodedID = encodeURIComponent(userID);
-        const requestUrl = `${baseUrl}/song/${encodedID}`;
+  const uploadSong = async (songData: SongUploadData) => {
+    const userID = "65647cd431a39aa197f9ebe7";
+    const encodedID = encodeURIComponent(userID);
+    const requestUrl = `${baseUrl}/song/${encodedID}`;
 
-        try {
-            const response = await axios.post(requestUrl, songData);
-            if (response) {
-                console.log('Song uploaded successfully!', response.data);
-            }
-        } catch (error) {
-            if (axios.isAxiosError(error) && error.response) {
-                console.error('Failed to upload song:', error.response.data.message || 'Unknown Error');
-            } else {
-                console.error('Failed to upload song:', error);
-            }
-        }
-    };
+    try {
+      const response = await axios.post(requestUrl, songData);
+      if (response) {
+        console.log("Song uploaded successfully!", response.data);
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        console.error(
+          "Failed to upload song:",
+          error.response.data.message || "Unknown Error"
+        );
+      } else {
+        console.error("Failed to upload song:", error);
+      }
+    }
+  };
 
-    return (
-        <ApiCallsContext.Provider value={{ uploadSong }}>
-            {children}
-        </ApiCallsContext.Provider>
-    );
+  return (
+    <ApiCallsContext.Provider value={{ uploadSong }}>
+      {children}
+    </ApiCallsContext.Provider>
+  );
 };
 
 // Custom Hook to use API Calls
 export const useApiCalls = () => {
-    const context = useContext(ApiCallsContext);
-    if (!context) {
-        throw new Error("useApiCalls must be used within an ApiCallsProvider");
-    }
-    return context;
+  const context = useContext(ApiCallsContext);
+  if (!context) {
+    throw new Error("useApiCalls must be used within an ApiCallsProvider");
+  }
+  return context;
 };
 
 export default ApiCallsProvider;
