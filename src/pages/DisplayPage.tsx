b57bd669166cb8fa78680bc93e3d1824708ae0cb
TheMisterPin <137160089+TheMisterPin@users.noreply.github.com>
2023-11-29 21:35:31 +0100

    [merge] albums forms

--- src/pages/DisplayPage.tsx
+++ src/pages/DisplayPage.tsx
@@ -4,7 +4,6 @@ import { PlayerDisplay } from "../components/playerDisplay/PlayerDisplay";
 import { useParams } from "react-router-dom";
 import type { Songs} from "../Types/SongsTypes"
 import { HeaderSection } from "../components";
-import { useSongs } from "../context/songContext/songContext";
 import { useApiCalls } from "../context/songContext/ApiCalls";
 
 
@@ -12,7 +11,6 @@ export const DisplayPage = () => {
   const { publicSongs } = useApiCalls()
   const { name } = useParams();
   const selectedSong = name
-    ? mySongs?.find((song: Songs) => song.name === name)
     ? publicSongs?.find((song: Songs) => song.name === name)
     : null;
   const defaultSong = publicSongs[0];
