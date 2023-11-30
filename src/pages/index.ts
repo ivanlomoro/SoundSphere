326ab64cac4bcd85b7b46029ccea7cf0e3023d4e
Ignacio Velazquez <ji.velazquezpp@gmail.com>
2023-11-30 10:48:08 +0100

    [FIX]: album creation and selection

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,6 @@
 export { FavoriteSongs } from "./FavoriteSongs";
-export {Home} from "./Home";
-export {UserPage} from "./UserPage";
-export {LandingPage} from "./LandingPage";
-export {DisplayPage} from './DisplayPage';
-export {AddMusicPage} from './AddMusicPageOLd';
-
+export { Home } from "./Home";
+export { UserPage } from "./UserPage";
+export { LandingPage } from "./LandingPage";
+export { DisplayPage } from "./DisplayPage";
+export { AddMusicPage } from "./AddMusicPage";
