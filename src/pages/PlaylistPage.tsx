8bc64f79d5fa9a76cf15ba6ad9a218201018e850
Ferran Cipres <ferrancipres@gmail.com>
2023-11-30 13:20:06 +0100

    updated PlaylistPage

--- src/pages/PlaylistPage.tsx
+++ src/pages/PlaylistPage.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { useInteractions } from '../context/userContext/InteractionContext'
-import { useRenderer } from '../hooks/useRenderer';
-import { HeaderSection } from '../components';
+// import React from 'react'
+// import { useInteractions } from '../context/userContext/InteractionContext'
+// import { useRenderer } from '../hooks/useRenderer';
+// import { HeaderSection } from '../components';
 
-const PlaylistPage = () => {
-    const { playlists, addToRecents, togglePlaylist, isPlaylist } = useInteractions();
-    const { renderSongs: renderPlaylistSongs } = useRenderer({ songs: playlists, togglePlaylist, isPlaylist, addToRecents, layout: "list" });
+// const PlaylistPage = () => {
+//     const { playlists, addToRecents, togglePlaylist, isPlaylist } = useInteractions();
+//     const { renderSongs: renderPlaylistSongs } = useRenderer({ songs: playlists, togglePlaylist, isPlaylist, addToRecents, layout: "list" });
 
-  return (
-    <>
-          <HeaderSection text="Playlists" />
-          <ul>
-            <h2> Playlists</h2>
-            { renderPlaylistSongs() }
-          </ul>
-    </>
-  );
-};
+//   return (
+//     <>
+//           <HeaderSection text="Playlists" />
+//           <ul>
+//             <h2> Playlists</h2>
+//             { renderPlaylistSongs() }
+//           </ul>
+//     </>
+//   );
+// };
 
-export default PlaylistPage
+// export default PlaylistPage
