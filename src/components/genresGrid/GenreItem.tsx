f4f96ad8600325093e4d25dc61b4c6371e503ff4
Kayn Mertens <adaygarcia336@gmail.com>
2023-12-01 10:52:05 +0100

    styled grid

--- src/components/genresGrid/GenreItem.tsx
+++ src/components/genresGrid/GenreItem.tsx
@@ -2,6 +2,10 @@ const imagenMierdaUrl =
   "https://res.cloudinary.com/dwlpzzkgh/image/upload/v1701370590/Hiphop-removebg-preview_qiktf5.png";
 
 const GenreItem = () => {
-  return <div>GenreItem</div>;
+  return(
+    <>
+    
+    </>
+  )
 };
 export default GenreItem;
