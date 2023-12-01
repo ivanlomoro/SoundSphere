1406e7c8b615449cea4636df6b0a29844d7818eb
Ignacio Velazquez <ji.velazquezpp@gmail.com>
2023-12-01 14:08:32 +0100

    [FIX]: boolean edit and delete buttons

--- src/components/card/SongCard.tsx
+++ src/components/card/SongCard.tsx
@@ -1,68 +1,121 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-import { Button } from '..'
-import { NavIcon } from '../NavBar/NavBar'
-import { Link } from 'react-router-dom'
-import { GridCard, ListCard, Card, GridImageContainer, GridCardImage, ListCardImage, CardImage, GridCardDescription, ListCardDescription, CardDescription, SongName, SongArtist, CommonButtonContainer, FullHeart, EmptyHeart, PlayButton, FaveButton, Plus, Minus } from './card.styled.components';
-import { AiOutlinePlayCircle } from 'react-icons/ai'
-import CardContainerButtons from './CardContainerButtons'
-import { SongCardProps } from '../../Types/SongsTypes'
-import { useInteractions } from '../../context/userContext/InteractionContext'
+import { Button } from "..";
+import { NavIcon } from "../NavBar/NavBar";
+import { Link } from "react-router-dom";
+import {
+  GridCard,
+  ListCard,
+  Card,
+  GridImageContainer,
+  GridCardImage,
+  ListCardImage,
+  CardImage,
+  GridCardDescription,
+  ListCardDescription,
+  CardDescription,
+  SongName,
+  SongArtist,
+  CommonButtonContainer,
+  FullHeart,
+  EmptyHeart,
+  PlayButton,
+  FaveButton,
+  Plus,
+  Minus,
+} from "./card.styled.components";
+import { AiOutlinePlayCircle } from "react-icons/ai";
+import CardContainerButtons from "./CardContainerButtons";
+import { SongCardProps } from "../../Types/SongsTypes";
+import { useInteractions } from "../../context/userContext/InteractionContext";
+import { useLocation } from "react-router-dom";
+import { MYSONGSPAGE } from "../../routes/paths";
 
-export function SongCard({ song,  variant = 'card' , isMySong}: SongCardProps) {
-	const CardComponent = variant === 'grid' ? GridCard : variant === 'list' ? ListCard : Card
-	const ImageComponent = variant === 'grid' ? GridCardImage : variant === 'list' ? ListCardImage : CardImage
-	const DescriptionComponent = variant === 'grid' ? GridCardDescription : variant === 'list' ? ListCardDescription : CardDescription
-	const { toggleFavorite, isFavorite, addToRecents, toggleSelected, isSelected, } = useInteractions()
-	
-	return (
-		<CardComponent>
-			{variant === 'grid' && (
-				<Link to={`/displaypage/${song.name}`}>
-					<GridImageContainer>
-						<ImageComponent src={song.thumbnail} alt={song.name} />
-						<PlayButton onClick={() => { addToRecents(song) }} />
-					</GridImageContainer>
-				</Link>
-			)}
+export function SongCard({ song, variant = "card" }: SongCardProps) {
+  const location = useLocation();
+  const CardComponent =
+    variant === "grid" ? GridCard : variant === "list" ? ListCard : Card;
+  const ImageComponent =
+    variant === "grid"
+      ? GridCardImage
+      : variant === "list"
+      ? ListCardImage
+      : CardImage;
+  const DescriptionComponent =
+    variant === "grid"
+      ? GridCardDescription
+      : variant === "list"
+      ? ListCardDescription
+      : CardDescription;
+  const {
+    toggleFavorite,
+    isFavorite,
+    addToRecents,
+    toggleSelected,
+    isSelected,
+  } = useInteractions();
 
-			{(variant != 'grid') && (
-				<ImageComponent src={song.thumbnail} alt={song.name} />
-			)}
+  return (
+    <CardComponent>
+      {variant === "grid" && (
+        <Link to={`/displaypage/${song.name}`}>
+          <GridImageContainer>
+            <ImageComponent src={song.thumbnail} alt={song.name} />
+            <PlayButton
+              onClick={() => {
+                addToRecents(song);
+              }}
+            />
+          </GridImageContainer>
+        </Link>
+      )}
 
-			<DescriptionComponent>
-				<div>
-					<SongName>{song.name}</SongName>
-					<SongArtist>{song.artist}</SongArtist>
-				</div>
-				{variant != 'grid' && (
-					<CommonButtonContainer>
-						<Link to={`/displaypage/${song.name}`}>
+      {variant != "grid" && (
+        <ImageComponent src={song.thumbnail} alt={song.name} />
+      )}
 
-							<Button
-								variant="StyledButtonNav"
-								content={<NavIcon icon={AiOutlinePlayCircle} />}
-								ariaLabel="Music Player"
-								onClick={() => { addToRecents(song) }}
-							/> </Link>
+      <DescriptionComponent>
+        <div>
+          <SongName>{song.name}</SongName>
+          <SongArtist>{song.artist}</SongArtist>
+        </div>
+        {variant != "grid" && (
+          <CommonButtonContainer>
+            <Link to={`/displaypage/${song.name}`}>
+              <Button
+                variant="StyledButtonNav"
+                content={<NavIcon icon={AiOutlinePlayCircle} />}
+                ariaLabel="Music Player"
+                onClick={() => {
+                  addToRecents(song);
+                }}
+              />{" "}
+            </Link>
 
-						<FaveButton onClick={() => { toggleFavorite(song) }}>
-							{isFavorite(song.id) ? <FullHeart /> : <EmptyHeart />}
-						</FaveButton>
-						
+            <FaveButton
+              onClick={() => {
+                toggleFavorite(song);
+              }}
+            >
+              {isFavorite(song.id) ? <FullHeart /> : <EmptyHeart />}
+            </FaveButton>
 
-
-						{/* <FavoriteButton onClick={() => { toggleFavorite(song) }}>
+            {/* <FavoriteButton onClick={() => { toggleFavorite(song) }}>
 						{isFavorite(song.id) ? <FullHeart /> : <EmptyHeart />}
 					</FavoriteButton>  */}
-						<FaveButton onClick={() => { toggleSelected(song) }}>
-							{isSelected(song.id) ? <Minus /> : <Plus />}
-						</FaveButton>
-					</CommonButtonContainer>)}
-			</DescriptionComponent>
-			{isMySong &&
-				<CardContainerButtons song={song} />}
-		</CardComponent>
-	)
+            <FaveButton
+              onClick={() => {
+                toggleSelected(song);
+              }}
+            >
+              {isSelected(song.id) ? <Minus /> : <Plus />}
+            </FaveButton>
+          </CommonButtonContainer>
+        )}
+      </DescriptionComponent>
+      {location.pathname === MYSONGSPAGE && (
+        <CardContainerButtons song={song} />
+      )}
+    </CardComponent>
+  );
 }
-
