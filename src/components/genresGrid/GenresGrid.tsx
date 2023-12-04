9465642f4ba3bfe5f9a5ce6ede3614e69404b30e
Kayn Mertens <adaygarcia336@gmail.com>
2023-12-04 12:34:58 +0100

    Arrelos en la pagina de search

--- src/components/genresGrid/GenresGrid.tsx
+++ src/components/genresGrid/GenresGrid.tsx
@@ -2,28 +2,30 @@ import { styled } from "styled-components";
 import { genres } from "../../interfaces/uploadTypes";
 import GenreCage from "./GenreCage";
 
+const predefinedColors = ["#FF5733", "#33FF57", "#5733FF", "#FF33A1", "#33A1FF"];
 
 const GridContainer = styled.div`
-margin-top: 1emn;
-display: grid;
-grid-template-columns: repeat(2);
-  column-gap: 5em;
+  margin-top: 1em;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr); // Set to two equal columns
+  column-gap: 3em;
   row-gap: 3em;
-max-width: 300px;
-margin-left: 1.5em;
+  max-width: 200px;
+  margin-left: 1em;
 `;
 
 const GenresGrid = () => {
   return (
     <>
-    <GridContainer>
-      {genres.map(genre => {
-        return(
-          <GenreCage key= {genre.id} genre= {genre} />
-        )
-      })}
-    </GridContainer>
+      <GridContainer>
+        {genres.map((genre, index) => {
+          const color = predefinedColors[index % predefinedColors.length];
+
+          return <GenreCage key={genre.id} genre={genre} color={color} />;
+        })}
+      </GridContainer>
     </>
-  )
+  );
 };
+
 export default GenresGrid;
