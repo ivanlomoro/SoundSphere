408adca06d28be78a831a2473aa145a91f93288e
Enric Broto Hernandez <enrique7siete@gmail.com>
2024-01-26 19:57:41 +0100

    [Fix] fecth data web open display album page

--- src/pages/albumDisplayPage.tsx
+++ src/pages/albumDisplayPage.tsx
@@ -5,12 +5,54 @@ import { useRenderer } from "../hooks/useRenderer";
 import { Album } from "./AddMusicPage";
 import { useApiCalls } from "../context/songContext/ApiCalls";
 import { AlbumHeader } from "../components/albumHeader/albumHeader";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Loader from "../components/Loader/Loader";
+
+const customAxios = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
 
 const AlbumDisplayPage = () => {
   const navigate = useNavigate();
   const { albumId } = useParams();
-  const { albums } = useApiCalls();
-  const album: Album | undefined = albums.find((album) => album.id === albumId);
+  // const { albums } = useApiCalls();
+  // const album: Album | undefined = albums.find((album) => album.id === albumId);
+  const [album, setAlbum] = useState<Album>()
+  const [isLoading, setLoading] = useState<boolean>(true);
+
+
+  const getAlbumsById = async (albumId: string) => {
+    try {
+      const response = await customAxios.get(
+        `album/${albumId}`
+      );
+      const album: Album = response.data;
+      setAlbum(album)
+    } catch (error) {
+      console.error("Failed to fetch Albums by Artist ID:", error);
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    const loadData = async () => {
+
+    try {
+       {albumId && await getAlbumsById(albumId) }
+
+    } catch (error) {
+      console.error("can't load album", error)
+    }
+    setLoading(false);
+  };
+  loadData();
+  }, [])
+
+  console.log("DISPLAY ALBUM:", album)
+
+
   const { renderSongs: renderAlbumSongs } = useRenderer({
     songs: album?.Song,
     layout: "list",
@@ -23,8 +65,23 @@ const AlbumDisplayPage = () => {
         withBackButton={true}
         arrowBackAction={() => navigate(-1)}
       />
-      {album ? <AlbumHeader album={album} /> : <h3> No hay canciones</h3>}
-      {album ? renderAlbumSongs() : <h3> No hay canciones</h3>}
+
+
+      {isLoading
+        ? <Loader />
+        :
+        <>
+       { album && <AlbumHeader album={album} />}
+       {album ? renderAlbumSongs() : (<h3> No hay canciones</h3>)}
+
+        </>
+      }
+
+      
+
+
+
+
     </>
   );
 };
