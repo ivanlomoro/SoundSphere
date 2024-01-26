7315e75ed82e2a4b1acf559915eca384608b3c4a
Kayn Mertens <adaygarcia336@gmail.com>
2024-01-26 11:35:41 +0100

    Album display page y api calls para el display de las musicas segun el album

--- src/pages/albumDisplayPage.tsx
+++ src/pages/albumDisplayPage.tsx
@@ -4,42 +4,23 @@ import { PlaylistContext } from "../context/playlistContext/PlayListContext";
 import { HeaderSection } from "../components";
 import { PlaylistType } from "../interfaces/PlaylistType";
 import { useRenderer } from "../hooks/useRenderer";
+import { useApiCalls } from "../context/songContext/ApiCalls";
 
 
 const AlbumDisplayPage = () => {
-  const { playlistId } = useParams();
-  const { getMusicByPlaylist, userPlaylists, songs } = useContext(PlaylistContext);
-  const [selectedPlaylist, setSelectedPlaylist] = useState<PlaylistType | null>(null);
-  const { renderSongs: renderPlaylistSongs  } = useRenderer({ songs: songs, layout: "list"});
-  useEffect(() => {
-    if (userPlaylists) {
-      const playlist = userPlaylists.find((playlist) => playlist.id === playlistId);
-      if (playlist) {
-        setSelectedPlaylist(playlist);
-        getMusicByPlaylist(playlist.id, playlist.playlistName);
-      }
-    }
-  }, [playlistId, userPlaylists]);
-
+ 
+  const { fetchSongsByAlbumId } = useApiCalls();
   const navigate = useNavigate();
-  console.log(selectedPlaylist)
+  
   return (
     <>
       <HeaderSection
-        text={selectedPlaylist?.playlistName}
+        text="Album"
         withBackButton={true}
         arrowBackAction={() => navigate(-1)}
       />
 
-      <div>
-        {selectedPlaylist && (
-          <>
-            <ul>
-              {renderPlaylistSongs()}
-            </ul>
-          </>
-        )}
-      </div>
+     
     </>
   );
 };
