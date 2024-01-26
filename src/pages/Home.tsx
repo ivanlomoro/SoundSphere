d9ca166478db21417c36c1e257663b64219a3991
TheMisterPin <137160089+TheMisterPin@users.noreply.github.com>
2024-01-26 10:56:50 +0100

    Fix Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,21 +5,19 @@ import {
 import GenreButtons from "../components/genresGrid/GenreButtons";
 import { useApiCalls } from "../context/songContext/ApiCalls";
 // import { useApiCalls } from "../context/songContext/ApiCalls";
-import { useInteractions } from "../context/userContext/InteractionContext";
-import { useRenderer } from "../hooks/useRenderer";
+
 // import { useContext } from "react";
 // import { PlaylistContext } from "../context/playlistContext/PlayListContext";
 import { ArtistCard } from "../components/card/ArtistCard";
 import { AlbumCard } from "../components/card/AlbumCard";
-import { useNavigate } from "react-router-dom";
+
 
 export const Home = () => {
   // const { publicSongs } = useApiCalls();
-  const { albums,artists,publicSongs } = useApiCalls();
-  console.log( 'albums',albums, 'artists',
-   artists,'publicSongs', publicSongs)
-  const{favorites, recents} = useInteractions()
-  // const { userPlaylists } = useContext(PlaylistContext);
+  const { albums, artists, publicSongs } = useApiCalls();
+  console.log('albums', albums, 'artists',
+    artists, 'publicSongs', publicSongs)
+
   // const { renderSongs: renderRecentsSongs } = useRenderer({
   //   songs: recents,
   //   layout: "grid",
@@ -32,15 +30,7 @@ export const Home = () => {
   // const { renderSongs: renderFavoriteSongs } = useRenderer({
   //   songs: favorites,
   //   layout: "card",
-  // });
-const{ renderSongs: renderPublicSongs } = useRenderer({
-    songs: publicSongs,
-    layout: "card",
-})
-  const { renderPlaylists: renderPlaylists } = useRenderer({
-    playlists: playlists,
-    layout: "card",
-  });
+  
 
 
   return (
@@ -52,13 +42,13 @@ const{ renderSongs: renderPublicSongs } = useRenderer({
       <h3>artists</h3>
       <div>
         <ScrollableRowComponent>
-          
+
           {/* <h2>Song List</h2> */}
           {/* <ScrollableRowComponent>{renderPublicSongs()}</ScrollableRowComponent> */}
           {artists.length > 0 && (
             <>
               {artists.map((artist) => (
-                <ArtistCard key={artist.id} artist={artist}/>
+                <ArtistCard key={artist.id} artist={artist} />
               ))}
             </>
           )}
@@ -83,13 +73,7 @@ const{ renderSongs: renderPublicSongs } = useRenderer({
           </>
         )}
 
-        {playlists.length > 0 && (
-          <>
-            <h3></h3>
-
-            <ScrollableRowComponent>{renderPublicSongs()}</ScrollableRowComponent>
-          </>
-        )}
+     
 
         {/* // Quiero hardcodear las playlists */}
         {/* {playlists.length > 0 && (
