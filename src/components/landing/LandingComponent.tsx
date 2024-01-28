33070b5dafa145644a80d0c3e2560d607dc99958
Ivan Martin Lomoro <ivanmartin_95@hotmail.com>
2024-01-28 18:42:25 +0100

    [Fix] : changes url

--- src/components/landing/LandingComponent.tsx
+++ src/components/landing/LandingComponent.tsx
@@ -7,25 +7,27 @@ import { Button } from "../button/Button";
 import styled from "styled-components";
 import { useAuth0 } from "@auth0/auth0-react";
 
-// import Swal from "sweetalert2";
-// import axios from "axios";
+import Swal from "sweetalert2";
+import axios from "axios";
 
 export const LandingComponent = () => {
   const { loginWithRedirect } = useAuth0();
 
-  // const handleClick = async () => {
-  //   try {
-  //     const response = await axios.get(import.meta.env.VITE_API_BASE_URL);
-  //     if (response.statusText === "OK") return loginWithRedirect();
-  //   } catch (error) {
-  //     Swal.fire({
-  //       title: "Server Error!",
-  //       text: "refresh and try again",
-  //       icon: "error",
-  //       confirmButtonText: "ok",
-  //     });
-  //   }
-  // };
+  const handleClick = async () => {
+    const serverURL = import.meta.env.VITE_API_BASE_URL;
+    console.log("Server URL", serverURL)
+    try {
+      const response = await axios.get(serverURL);
+      if (response.statusText === "OK") return loginWithRedirect();
+    } catch (error) {
+      Swal.fire({
+        title: "Server Error!",
+        text: "refresh and try again",
+        icon: "error",
+        confirmButtonText: "ok",
+      });
+    }
+  };
 
   const StyledMain = styled.main`
     width: var(--w-full);
@@ -47,7 +49,7 @@ export const LandingComponent = () => {
           <StyledMain>
             <Button
               content="Login"
-              onClick={(): Promise<void> => loginWithRedirect()}
+              onClick={(): Promise<void> => handleClick()}
             />
           </StyledMain>
         </StyledDiv>
